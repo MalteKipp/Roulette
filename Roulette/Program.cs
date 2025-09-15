6e943674aa900c80a8869bd3f349b02d76017f87
MalteKipp <14MK1029@amal.se>
2025-09-15 11:00:59 +0200

    Add project files.

---
+++ Roulette/Program.cs
@@ -0,0 +1,49 @@
+﻿//fråga ochg spara svaren
+Console.WriteLine("Enter a number between 0-36 or guess red/green");
+Console.ReadLine();
+string colourguess;
+
+
+
+//slut på frågandet
+
+
+//funktionen
+
+
+while(true)
+    {
+    Random rand = new Random();
+    int roulette = rand.Next(0, 36);
+    int guess = Convert.ToInt32(Console.ReadLine());
+
+int colour = rand.Next(1, 2);
+
+        if(guess < 0)
+    {
+      Console.WriteLine("That number is not on the spectrum guess again");
+    }
+        else if (guess > 36)
+        {
+            Console.WriteLine("That number is not on the spectrum guess again");
+        }
+
+
+        else if (guess == roulette)
+    {
+        Console.WriteLine("You won Congratulations");
+    }
+    else
+    {
+        Console.WriteLine("You lost the number was " + roulette);
+    }
+    if (colour == colourguess)
+{
+    Console.WriteLine("Thats the right colour");
+}
+else
+{
+    Console.WriteLine("Thats the wrong colour");
+}
+}
+    
