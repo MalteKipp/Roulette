aa67ea4029eab0d6283f383c378ce7b2a0430fa8
MalteKipp <14MK1029@amal.se>
2025-09-22 11:00:05 +0200

    VIP

--- Roulette/Program.cs
+++ Roulette/Program.cs
@@ -11,12 +11,19 @@ bool Colour = false;
 
 bool Number = false;
 bool isPlaying = true;
-int Balance = 100;
+int Balance = 1000;
+ bool shop = false;
+
+
+int vip = 150;
+
 while (isPlaying)
-{ 
+{
+    
     Console.WriteLine("Welcome to the roulette table!");
     Console.WriteLine("Press 1 to bet on colours");
     Console.WriteLine("Press 2 to bet between 1-36");
+    Console.WriteLine("Press 3 to open the shop");
     Console.WriteLine("Balance:" + Balance + "$");
     
     char input = Console.ReadKey().KeyChar;
@@ -32,8 +39,14 @@ while (isPlaying)
         Number = true;
         Console.Clear();
     }
+    else if (input == '3')
+    {
+        shop = true;
+        Console.Clear();
+    }
     else
     {
+        Console.Clear();
         Console.WriteLine("Not an option");
     }
 
@@ -46,16 +59,22 @@ while (isPlaying)
         Console.WriteLine("Type Menu = Return to menu");
         Console.WriteLine("Enter bet amount and press enter");
 
-        Console.WriteLine("Type Red or Black and press enter");
+        Console.WriteLine("Type Red, Black or Green and press enter");
 
         Random rand = new Random();
 
         int BetAmount = Convert.ToInt32(Console.ReadLine());
         int roulette = rand.Next(1, 3);
+        int green = rand.Next(0, 37);
 
         string guess = Console.ReadLine();
 
-
+        if (BetAmount > Balance)
+        {
+            Console.Clear();
+            Console.WriteLine("Cheater lets try again (you bet more then on your balance)");
+            break;
+        }
         if (guess == "Red")
         {
 
@@ -69,7 +88,7 @@ while (isPlaying)
             else
             {
                 Console.WriteLine("you lost");
-                Balance = Balance - BetAmount;
+                 Balance = Balance - BetAmount;
                 Console.WriteLine(Balance +"$");
             }
 
@@ -81,11 +100,35 @@ while (isPlaying)
             if (roulette == 2)
             {
                 Console.WriteLine("you won!");
+                Balance = Balance + BetAmount;
+                Console.WriteLine(Balance + "$");
             }
             else
             {
                 Console.WriteLine("you lost");
+                Balance = Balance - BetAmount;
+                Console.WriteLine(Balance + "$");
             }
+
+
+        }
+        else if (guess == "Green")
+        {
+
+            if (green == 4)
+            {
+                Console.WriteLine("you won!");
+                Balance = Balance + BetAmount*35;
+                Console.WriteLine(Balance + "$");
+            }
+            else
+            {
+                Console.WriteLine("you lost");
+                Balance = Balance - BetAmount;
+                Console.WriteLine(Balance + "$");
+            }
+
+
         }
         else if (guess != "Red" || guess != "Green")
         {
@@ -101,6 +144,8 @@ while (isPlaying)
 
         }
        Console.Clear();
+
+       
     }
 
 
@@ -119,8 +164,14 @@ while (isPlaying)
         Random rand = new Random();
 
 
-        
-        int roulette = rand.Next(0, 36);
+        if (BetAmount > Balance)
+        {
+            Console.Clear();
+            Console.WriteLine("Cheater lets try again (you bet more then on your balance)");
+            break;
+        }
+
+        int roulette = rand.Next(0, 37);
         int guessNumb = Convert.ToInt32(guess);
 
         if (guessNumb < 0)
@@ -162,6 +213,58 @@ while (isPlaying)
             }
         
         Console.Clear();
+
+
     }
-    
+    while (shop)
+    {
+        
+
+        Console.WriteLine("Welcome to the shop here you can buy upgrades");
+        Console.WriteLine("Type out what you want to buy and hit enter!");
+        Console.WriteLine("(1) VIP (2x money): 150$");
+        Console.WriteLine("balance:" + Balance + "$");
+        string ShopInput = Console.ReadLine();
+        
+
+
+        if (ShopInput == "Menu")
+        {
+            Number = false;
+            Colour = false;
+            shop = false;
+            Console.Clear();
+
+        }
+
+        
+
+        else if (ShopInput == "1") 
+        {
+            if (Balance > vip)
+            {
+                Console.Clear();
+                Console.WriteLine("You purchased VIP!");
+                Balance = Balance - vip;
+                vip = 1;
+
+            }
+            else if (Balance < vip) 
+            {
+                Console.Clear();
+                Console.WriteLine("You cant afford that");
+            }
+            else if (ShopInput != "1" || ShopInput!= "Menu")
+            {
+                Console.Clear();
+            }
+            
+
+
+        }
+
+    }
+
+
 }
+
