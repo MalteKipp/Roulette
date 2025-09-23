3b57463891231674a7c85ee6613ef9baaeb6d19d
MalteKipp <14MK1029@amal.se>
2025-09-23 18:49:32 +0200

    fix

--- Roulette/Program.cs
+++ Roulette/Program.cs
@@ -1,25 +1,33 @@
-﻿
+﻿Console.BackgroundColor = ConsoleColor.Green;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.Clear();
+Console.WriteLine("Welcome to roulette RNG");
+Thread.Sleep(2000);
+Console.Clear();
+Console.WriteLine("Your goal is to reach 10k and finish the game!");
+Thread.Sleep(3000);
+Console.Clear(); 
+Console.BackgroundColor = ConsoleColor.DarkRed;
+Console.ForegroundColor = ConsoleColor.White;
+Console.Clear();
 
 
-
-
-
-
-
-
+bool isPlaying = true;
 bool Colour = false;
 
 bool Number = false;
-bool isPlaying = true;
+
 int Balance = 1000;
  bool shop = false;
 
 
 int vip = 150;
 
+
+
 while (isPlaying)
 {
-    
+    Random rand = new Random();
     Console.WriteLine("Welcome to the roulette table!");
     Console.WriteLine("Press 1 to bet on colours");
     Console.WriteLine("Press 2 to bet between 1-36");
@@ -61,7 +69,7 @@ while (isPlaying)
 
         Console.WriteLine("Type Red, Black or Green and press enter");
 
-        Random rand = new Random();
+       
 
         int BetAmount = Convert.ToInt32(Console.ReadLine());
         int roulette = rand.Next(1, 3);
@@ -83,6 +91,11 @@ while (isPlaying)
                 Console.WriteLine(" you won!");
                 
                 Balance = Balance + BetAmount;
+                
+                if(vip == 1)
+                {
+                    Balance = Balance + BetAmount;
+                }
                 Console.WriteLine(Balance+"$");
             }
             else
@@ -101,6 +114,11 @@ while (isPlaying)
             {
                 Console.WriteLine("you won!");
                 Balance = Balance + BetAmount;
+                
+                if (vip == 1)
+                {
+                    Balance = Balance + BetAmount;
+                }
                 Console.WriteLine(Balance + "$");
             }
             else
@@ -161,7 +179,7 @@ while (isPlaying)
         Console.WriteLine("Guess between 1-36 and press enter:");
         int BetAmount = Convert.ToInt32(Console.ReadLine());
         string guess = Console.ReadLine();
-        Random rand = new Random();
+       
 
 
         if (BetAmount > Balance)
@@ -191,6 +209,10 @@ while (isPlaying)
 
            
             Balance = Balance + BetAmount * 35;
+            if (vip == 1)
+            {
+                Balance = Balance + BetAmount * 35;
+            }
             Console.WriteLine(Balance + "$");
         }
 
@@ -218,11 +240,12 @@ while (isPlaying)
     }
     while (shop)
     {
-        
 
+        
         Console.WriteLine("Welcome to the shop here you can buy upgrades");
         Console.WriteLine("Type out what you want to buy and hit enter!");
         Console.WriteLine("(1) VIP (2x money): 150$");
+        Console.WriteLine("(2) Ultra gamble (50% to x2 balance 50% to set balance to 0): free");
         Console.WriteLine("balance:" + Balance + "$");
         string ShopInput = Console.ReadLine();
         
@@ -241,30 +264,62 @@ while (isPlaying)
 
         else if (ShopInput == "1") 
         {
+
             if (Balance > vip)
             {
-                Console.Clear();
+                
                 Console.WriteLine("You purchased VIP!");
+                Thread.Sleep(1000);
+                Console.Clear();
                 Balance = Balance - vip;
                 vip = 1;
 
             }
             else if (Balance < vip) 
-            {
+            {   Console.WriteLine("You cant afford that");
+                Thread.Sleep(1000);
                 Console.Clear();
-                Console.WriteLine("You cant afford that");
+                
             }
-            else if (ShopInput != "1" || ShopInput!= "Menu")
-            {
-                Console.Clear();
-            }
+            
             
 
 
+        }
+        else if (ShopInput == "2")
+        {
+            
+           
+            int gambler = rand.Next(1, 3);
+            if (gambler == 1)
+            {
+                Console.WriteLine("Lucky there");
+                Balance = Balance * 2;
+                Thread.Sleep(1000);
+                Console.Clear();
+
+            }
+            else if (gambler == 2)
+            {
+                Console.WriteLine("Game over goodluck next time!");
+                Balance = 0;
+                Thread.Sleep (1000);
+                Console.Clear();
+               
+                
+            }
+        }
+        else if (ShopInput != "1" && ShopInput != "Menu" && ShopInput != "2")
+        {
+            Console.WriteLine("Try a correct input this time");
+            Thread.Sleep(1000);
+            Console.Clear();
         }
 
     }
 
 
 }
+
+
 
